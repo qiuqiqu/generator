d367579698a4678a14bb4ab6bab81fb311326581
qiuqiqu <1961237983@qq.com>
2024-01-18 14:06:46 +0800

    优化动态文件代码抽取成一个方法

--- generator-basic/src/main/java/com/gygenerator/DynamicGenerator.java
+++ generator-basic/src/main/java/com/gygenerator/DynamicGenerator.java
@@ -13,28 +13,49 @@ import java.io.Writer;
 
 public class DynamicGenerator {
     public static void main(String[] args) throws IOException, TemplateException {
+        String projectPath = System.getProperty("user.dir");
+        String inputPath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String outputPath = projectPath + File.separator + "MainTemplate.java";
+        MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
+        mainTemplateConfig.setAuthor("yupi");
+        mainTemplateConfig.setLoop(false);
+        mainTemplateConfig.setOutputText("求和结果：");
+        doGenerate(inputPath, outputPath, mainTemplateConfig);
+    }
+
+    /**
+     * 生成文件
+     *
+     * @param inputPath 模板文件输入路径
+     * @param outputPath 输出路径
+     * @param model 数据模型
+     * @throws IOException
+     * @throws TemplateException
+     */
+    public static void doGenerate(String inputPath, String outputPath, Object model) throws IOException, TemplateException {
         // new 出 Configuration 对象，参数为 FreeMarker 版本号
         Configuration configuration = new Configuration(Configuration.VERSION_2_3_32);
 
         // 指定模板文件所在的路径
-        configuration.setDirectoryForTemplateLoading(new File("src/main/resources/templates"));
+        File templateDir = new File(inputPath).getParentFile();
+        configuration.setDirectoryForTemplateLoading(templateDir);
 
         // 设置模板文件使用的字符集
         configuration.setDefaultEncoding("utf-8");
 
         // 创建模板对象，加载指定模板
-        Template template = configuration.getTemplate("MainTemplate.java.ftl");
+        String templateName = new File(inputPath).getName();
+        Template template = configuration.getTemplate(templateName);
 
         // 创建数据模型
         MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
-        mainTemplateConfig.setAuthor("秋秋南");
-        // 不使用循环
-        mainTemplateConfig.setLoop(true);
+        mainTemplateConfig.setAuthor("yupi");
+        mainTemplateConfig.setLoop(false);
         mainTemplateConfig.setOutputText("求和结果：");
 
         // 生成
-        Writer out = new FileWriter("MainTemplate.java");
-        template.process(mainTemplateConfig, out);
+        Writer out = new FileWriter(outputPath);
+        template.process(model, out);
 
         // 生成文件后别忘了关闭哦
         out.close();
