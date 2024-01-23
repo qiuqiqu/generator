3c0b837a67ae4173bfd069eae430f914975d7648
qiuqiqu <1961237983@qq.com>
2024-01-23 14:04:32 +0800

    功能优化(增加项目介绍文件,制作精简版代码生成器)

--- generator-maker/src/main/java/com/gy/maker/generator/main/MainGenerator.java
+++ generator-maker/src/main/java/com/gy/maker/generator/main/MainGenerator.java
@@ -105,6 +105,13 @@ public class MainGenerator {
         outputFilePath = outputPath + File.separator + "pom.xml";
         DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
 
+        //--------------------------------------------------------------------------------------------------------------
+
+        // README.md
+        inputFilePath = inputResourcePath + File.separator + "templates/README.md.ftl";
+        outputFilePath = outputPath + File.separator + "README.md";
+        DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
+
         // 构建 jar 包
         JarGenerator.doGenerate(outputPath);
         //--------------------------------------------------------------------------------------------------------------
@@ -114,5 +121,18 @@ public class MainGenerator {
         String jarName = String.format("%s-%s-jar-with-dependencies.jar", meta.getName(), meta.getVersion());
         String jarPath = "target/" + jarName;
         ScriptGenerator.doGenerate(shellOutputFilePath, jarPath);
+
+        // 生成精简版的程序（产物包）
+        String distOutputPath = outputPath + "-dist";
+        // - 拷贝 jar 包
+        String targetAbsolutePath = distOutputPath + File.separator + "target";
+        FileUtil.mkdir(targetAbsolutePath);
+        String jarAbsolutePath = outputPath + File.separator + jarPath;
+        FileUtil.copy(jarAbsolutePath, targetAbsolutePath, true);
+        // - 拷贝脚本文件
+        FileUtil.copy(shellOutputFilePath, distOutputPath, true);
+        FileUtil.copy(shellOutputFilePath + ".bat", distOutputPath, true);
+        // - 拷贝源模板文件
+        FileUtil.copy(sourceCopyDestPath, distOutputPath, true);
     }
 }
