f8d75b49e8c80a7af5854b5b17e7ead2c20698f8
qiuqiqu <1961237983@qq.com>
2024-03-25 17:03:46 +0800

    文件打包压缩

--- generator-maker/src/main/java/com/gy/maker/Main.java
+++ generator-maker/src/main/java/com/gy/maker/Main.java
@@ -1,13 +1,15 @@
 package com.gy.maker;
 
+import com.gy.maker.generator.main.GenerateTemplate;
 import com.gy.maker.generator.main.MainGenerator;
+import com.gy.maker.generator.main.ZipGenerator;
 import freemarker.template.TemplateException;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
-        MainGenerator mainGenerator = new MainGenerator();
-        mainGenerator.doGenerate();
+        GenerateTemplate generateTemplate = new ZipGenerator();
+        generateTemplate.doGenerate();
     }
 }
--- generator-maker/src/main/java/com/gy/maker/generator/main/GenerateTemplate.java
+++ generator-maker/src/main/java/com/gy/maker/generator/main/GenerateTemplate.java
@@ -3,6 +3,7 @@ package com.gy.maker.generator.main;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ClassPathResource;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.ZipUtil;
 import com.gy.maker.generator.JarGenerator;
 import com.gy.maker.generator.ScriptGenerator;
 import com.gy.maker.generator.file.DynamicFileGenerator;
@@ -156,12 +157,14 @@ public abstract class GenerateTemplate {
 
     /**
      * 生成精简版程序
+     *
      * @param outputPath
      * @param sourceCopyDestPath
      * @param jarPath
      * @param shellOutputFilePath
+     * @return 产物包路径
      */
-    protected void buildDist(String outputPath, String sourceCopyDestPath, String jarPath, String shellOutputFilePath) {
+    protected String buildDist(String outputPath, String sourceCopyDestPath, String jarPath, String shellOutputFilePath) {
         String distOutputPath = outputPath + "-dist";
         // 拷贝 jar 包
         String targetAbsolutePath = distOutputPath + File.separator + "target";
@@ -172,5 +175,18 @@ public abstract class GenerateTemplate {
         FileUtil.copy(shellOutputFilePath, distOutputPath, true);
         // 拷贝源模板文件
         FileUtil.copy(sourceCopyDestPath, distOutputPath, true);
+        return distOutputPath;
+    }
+
+    /**
+     * 制作压缩包
+     *
+     * @param outputPath
+     * @return 压缩包路径
+     */
+    protected String buildZip(String outputPath) {
+        String zipPath = outputPath + ".zip";
+        ZipUtil.zip(outputPath, zipPath);
+        return zipPath;
     }
 }
