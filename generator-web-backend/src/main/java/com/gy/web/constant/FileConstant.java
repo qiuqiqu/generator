3904dd8a88be705a31038ff2d50fa8398012643c
qiuqiqu <1961237983@qq.com>
2024-03-25 17:02:41 +0800

    文件上传和下载

--- generator-web-backend/src/main/java/com/gy/web/constant/FileConstant.java
+++ generator-web-backend/src/main/java/com/gy/web/constant/FileConstant.java
@@ -10,5 +10,5 @@ public interface FileConstant {
      * COS 访问地址
      * todo 需替换配置
      */
-    String COS_HOST = "https://yupi.icu";
+    String COS_HOST = "https://qiu-qiu-nan-1322461037.cos.ap-shanghai.myqcloud.com";
 }
--- generator-web-backend/src/main/java/com/gy/web/controller/FileController.java
+++ generator-web-backend/src/main/java/com/gy/web/controller/FileController.java
@@ -1,27 +1,31 @@
 package com.gy.web.controller;
 
 import cn.hutool.core.io.FileUtil;
+import com.gy.web.annotation.AuthCheck;
 import com.gy.web.common.BaseResponse;
 import com.gy.web.common.ErrorCode;
 import com.gy.web.common.ResultUtils;
 import com.gy.web.constant.FileConstant;
+import com.gy.web.constant.UserConstant;
 import com.gy.web.exception.BusinessException;
 import com.gy.web.manager.CosManager;
 import com.gy.web.model.dto.file.UploadFileRequest;
 import com.gy.web.model.entity.User;
 import com.gy.web.model.enums.FileUploadBizEnum;
 import com.gy.web.service.UserService;
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.model.COSObjectInputStream;
+import com.qcloud.cos.utils.IOUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 /**
@@ -102,6 +106,72 @@ public class FileController {
             }
             if (!Arrays.asList("jpeg", "jpg", "svg", "png", "webp").contains(fileSuffix)) {
                 throw new BusinessException(ErrorCode.PARAMS_ERROR, "文件类型错误");
+            }
+        }
+    }
+
+    /**
+     * 测试文件上传
+     *
+     * @param multipartFile
+     * @return
+     */
+    //@AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @PostMapping("/test/upload")
+    public BaseResponse<String> testUploadFile(@RequestPart("file") MultipartFile multipartFile) {
+        // 文件目录
+        String filename = multipartFile.getOriginalFilename();
+        String filepath = String.format("/test/%s", filename);
+        File file = null;
+        try {
+            // 上传文件
+            file = File.createTempFile(filepath, null);
+            multipartFile.transferTo(file);
+            cosManager.putObject(filepath, file);
+            // 返回可访问地址
+            return ResultUtils.success(filepath);
+        } catch (Exception e) {
+            log.error("file upload error, filepath = " + filepath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "上传失败");
+        } finally {
+            if (file != null) {
+                // 删除临时文件
+                boolean delete = file.delete();
+                if (!delete) {
+                    log.error("file delete error, filepath = {}", filepath);
+                }
+            }
+        }
+    }
+
+    /**
+     * 测试文件下载
+     *
+     * @param filepath
+     * @param response
+     * @return
+     */
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @GetMapping("/test/download/")
+    public void testDownloadFile(String filepath, HttpServletResponse response) throws IOException {
+        COSObjectInputStream cosObjectInput = null;
+        try {
+            COSObject cosObject = cosManager.getObject(filepath);
+            cosObjectInput = cosObject.getObjectContent();
+            // 处理下载到的流
+            byte[] bytes = IOUtils.toByteArray(cosObjectInput);
+            // 设置响应头
+            response.setContentType("application/octet-stream;charset=UTF-8");
+            response.setHeader("Content-Disposition", "attachment; filename=" + filepath);
+            // 写入响应
+            response.getOutputStream().write(bytes);
+            response.getOutputStream().flush();
+        } catch (Exception e) {
+            log.error("file download error, filepath = " + filepath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "下载失败");
+        } finally {
+            if (cosObjectInput != null) {
+                cosObjectInput.close();
             }
         }
     }
